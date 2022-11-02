17c3625bc65ab5a9adf14585de1cb77e44c45291
k-koech <koechkelvin97@gmail.com>
2022-11-02 05:48:47 +0300

    iniital commit

---
+++ Sorted square array.py
@@ -0,0 +1,16 @@
+''' Write a function that takes in a non-empty array of integers that are sorted in ascending order 
+and returns a new array of the same length with the squares of the original integers also sorted 
+in ascending order. '''
+
+'''For example:
+      array = [1,  2,  3,  5,  6,  8,  9] 
+Returns:
+      [1,  4,  9,  25,  36,  64,  81]'''
+
+def sortedSquareArray(array):    
+    for i in ara 
+    return "xxx"
+
+print(sortedSquareArray([-9,3,7,6]))
+
+      
